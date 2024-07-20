914836673eb8f3fdb3f5a7880f983c291f607dfd
Nina Bobyleva <2847171@mail.ru>
2024-07-20 19:09:55 +0300

    added-handleFavorite

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,14 +2,25 @@ import styles from "./menu.module.css";
 import Link from "next/link";
 import { logout } from "@/store/features/userSlice";
 import { useAppDispatch, useAppSelector } from "@/store/store";
+import { useRouter } from "next/navigation";
 
 export function Menu() {
   const dispatch = useAppDispatch();
   const { user } = useAppSelector((state) => state.user);
-  
+  const router = useRouter();
+
   const handleLogout = () => {
     dispatch(logout());
   };
+
+  const handleFavorite = () => {
+    if (user) {
+      router.push("/favorite");
+    } else {
+      alert("Вы не авторизованы");
+    }
+  };
+
   return (
     <div className={styles.navMenu}>
       <ul className={styles.menuList}>
@@ -18,10 +29,10 @@ export function Menu() {
             Главное
           </Link>
         </li>
-        <li className={styles.menuItem}>
-          <Link className={styles.menuLink} href="/favorite">
+        <li onClick={handleFavorite} className={styles.menuItem}>
+          <a onClick={handleLogout} className={styles.menuLink}>
             Мой плейлист
-          </Link>
+          </a>
         </li>
         <li className={styles.menuItem}>
           {user ? (
