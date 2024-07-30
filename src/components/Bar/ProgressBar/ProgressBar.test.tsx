4714aac88bdd75ff16aa09fd26029176d7382c5c
Nina Bobyleva <2847171@mail.ru>
2024-07-30 19:55:21 +0300

    correction

--- src/components/Bar/ProgressBar/ProgressBar.test.tsx
+++ src/components/Bar/ProgressBar/ProgressBar.test.tsx
@@ -4,9 +4,16 @@ import ProgressBar from "./ProgressBar";
 describe("Компонент ProgressBar", () => {
   it("Успешный рендер ProgressBar", () => {
     const handleSeek = jest.fn();
-    const test = renderer.create(
-      <ProgressBar max={135.444898} value={2.377663} step={0.01} onChange={handleSeek} />
-    ).toJSON();
+    const test = renderer
+      .create(
+        <ProgressBar
+          max={135.444898}
+          value={2.377663}
+          step={0.01}
+          onChange={handleSeek}
+        />
+      )
+      .toJSON();
     expect(test).toMatchSnapshot();
   });
 });
